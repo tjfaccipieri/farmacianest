9a2ff5806a0d77350f78663f71111ae68863445a
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2025-08-04 21:09:08 -0300

    preparando deploy

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   process.env.TZ = '-03:00';
+
+  const config = new DocumentBuilder()
+    .setTitle('Farmacinha da turma JS07')
+    .setDescription('Backend do projeto da Farmacia Backend')
+    .setContact(
+      'Thiago Faccipieri',
+      'http://www.github.com/tjfaccipieri',
+      'thiago.faccipieri@gmail.com',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/swagger', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
 
